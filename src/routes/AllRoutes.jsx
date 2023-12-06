945ac58ff9fa17c916da15e33e84bb7d2aea6750
abhik345 <abhikdas345@gmail.com>
2023-12-06 10:57:20 +0530

    added protected routes

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,33 +1,122 @@
-import React from 'react';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
-import { Home, Login, PageNotFound,AssignmentListing,CreateMaster,CreateUser, User } from "../pages"
-import { CreateCategory } from '../pages/master/CreateCategory';
-import CreateLocation from '../pages/master/CreateLocation';
-import CreateSubcategory from '../pages/master/CreateSubcategory';
-import CreateArea from '../pages/master/CreateArea';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import {
+  Home,
+  Login,
+  PageNotFound,
+  AssignmentListing,
+  CreateMaster,
+  CreateUser,
+  User,
+} from "../pages";
+import { CreateCategory } from "../pages/master/CreateCategory";
+import CreateLocation from "../pages/master/CreateLocation";
+import CreateSubcategory from "../pages/master/CreateSubcategory";
+import CreateArea from "../pages/master/CreateArea";
 
 const AllRoutes = () => {
   return (
-   <>
-   
-   <Router>
-    <Routes>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/all-assignment' element={<AssignmentListing/>}/>
-        <Route path='/create-category' element={<CreateCategory/>}/>
-        <Route path='/create-location' element={<CreateLocation/>}/>
-        <Route path='/create-subcategory' element={<CreateSubcategory/>}/>
-        <Route path='/create-area' element={<CreateArea/>}/>
-        <Route path='/users' element={<User/>}/>
-        <Route path='/create-master' element={<CreateMaster/>}/>
-        <Route path='/create-user' element={<CreateUser/>}/>
-        <Route path='/' element={<Login/>}/>
-        <Route path='*' element={<PageNotFound/>}/>
-    </Routes>
-   </Router>
-   
-   </>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/all-assignment"
+            element={
+              <ProtectedRoutes>
+                <AssignmentListing />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-category"
+            element={
+              <ProtectedRoutes>
+                <CreateCategory />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-location"
+            element={
+              <ProtectedRoutes>
+                <CreateLocation />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-subcategory"
+            element={
+              <ProtectedRoutes>
+                <CreateSubcategory />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-area"
+            element={
+              <ProtectedRoutes>
+                <CreateArea />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoutes>
+                <User />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-master"
+            element={
+              <ProtectedRoutes>
+                <CreateMaster />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/create-user"
+            element={
+              <ProtectedRoutes>
+                <CreateUser />
+              </ProtectedRoutes>
+            }
+          />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="*"
+            element={
+              <ProtectedRoutes>
+                <PageNotFound />
+              </ProtectedRoutes>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default AllRoutes
+export default AllRoutes;
+
+export function ProtectedRoutes(props) {
+  if (localStorage.getItem("user_id") || localStorage.getItem("vendor_id")) {
+    return props.children;
+  } else {
+    return <Navigate to="/" />;
+  }
+}
